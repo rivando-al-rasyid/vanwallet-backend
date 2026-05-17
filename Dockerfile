222a677180f0d4959583894791d0e3bc41a097a8
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-05-17 20:25:07 +0700

    ref : remove un used file

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM postgres:18.4-alpine3.23
-
-ENV POSTGRES_USER=vando
-ENV POSTGRES_DB=vanwalletdb
-
-COPY ./database/backenvanwallet.sql /docker-entrypoint-initdb.d/
-
-EXPOSE 5432
--- internals/dto/response.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type Response struct {
-	Message string `json:"message"`
-	Data    any    `json:"data"`
-	Success bool   `json:"isSuccess"`
-	Error   string `json:"error"`
-}
