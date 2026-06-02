71cd35c057f77bab65fb8f1f43210c3ad9fabce6
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-03 02:14:19 +0700

    ref : middle ware and  docker version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM golang:1.26.3-alpine3.23 AS builder
+# ─── Stage 1: Builder ────────────────────────────────────────────────────────
+FROM golang:1.24-alpine AS builder
 
 WORKDIR /app
 
@@ -7,7 +8,6 @@ RUN apk add --no-cache git
 RUN go install github.com/swaggo/swag/cmd/swag@latest
 
 COPY go.mod go.sum ./
-
 RUN go mod download
 
 COPY . .
@@ -18,8 +18,7 @@ RUN CGO_ENABLED=0 GOOS=linux go build \
     -ldflags="-s -w" \
     -o /vanwallet ./cmd/
 
-
-FROM alpine:3.23
+FROM alpine:3.21
 
 WORKDIR /app
 
--- internals/router/main.router.go
+++ internals/router/main.router.go
@@ -5,13 +5,14 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 	_ "github.com/rivando-al-rasyid/vanwallet-backend/docs"
+	"github.com/rivando-al-rasyid/vanwallet-backend/internals/middleware"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func MainRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	// router.Use(middleware.CORSMiddleware)
+	router.Use(middleware.CORSMiddleware)
 	router.Static("/img", "public/img")
 	AuthRouter(router, db, rdb)
 	ProfileRouter(router, db)
