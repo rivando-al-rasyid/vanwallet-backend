909bb377fd1782d44eea757ad507ee78c4a98e51
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-22 11:00:36 +0700

    feat : redis

--- internals/config/redis.go
+++ internals/config/redis.go
@@ -3,16 +3,36 @@ package config
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func ConnectRedis() (*redis.Client, error) {
-	rc := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("RDB_ADDR"),
-		Username: os.Getenv("RDB_USER"),
-		Password: os.Getenv("RDB_PASS"),
-	})
-	sc := rc.Ping(context.Background())
-	return rc, sc.Err()
+	redisAddr := os.Getenv("RDB_ADDR")
+
+	var opt *redis.Options
+	var err error
+
+	if strings.HasPrefix(redisAddr, "redis://") || strings.HasPrefix(redisAddr, "rediss://") {
+		opt, err = redis.ParseURL(redisAddr)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		opt = &redis.Options{
+			Addr:     redisAddr,
+			Username: os.Getenv("RDB_USER"),
+			Password: os.Getenv("RDB_PASS"),
+		}
+	}
+
+	rc := redis.NewClient(opt)
+
+	err = rc.Ping(context.Background()).Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return rc, nil
 }
